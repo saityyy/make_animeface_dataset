17b8d43bef7d2180c39af6cf43a46e5c2aab9900
saityyy <sec200091@gmail.com>
2021-10-24 09:37:30 +0900

    make CheckFace class

--- manage_data/check_face.py
+++ manage_data/check_face.py
@@ -12,52 +12,65 @@ from PIL import Image, ImageTk
 
 # 指定のフォルダから画像をランダムに参照する
 FROMPATH = "D:/Project/MLProject/DataScience/data/GirlsImage"
-TOPATH = "./manage_data/data/temp"
-
-root = tk.Tk()
-image_scandir_list = list(os.scandir(FROMPATH))
+TOPATH = os.path.join(os.path.dirname(__file__), "data/temp")
 
 
-def random_name():
+class CheckFace:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.image_scandir_list = list(os.scandir(FROMPATH))
+        self.canvas = tk.Canvas(
+            bg="black", width=2000, height=2000)
+        self.add_image_count = 0
+        self.image_num = len(os.listdir(TOPATH))
+
+    @property
+    def current_image_num(self):
+        return self.add_image_count+self.image_num
+
+    def __call__(self):
+        self.canvas.place(x=0, y=0)
+        self.img = self.select_image()
+        self.canvas.create_image(0, 0, image=self.img, anchor=tk.NW)
+        self.root.geometry("{}x{}".format(800, 800))
+        self.canvas.bind('<Button-1>', self.add_image)  # 左クリック
+        self.canvas.bind('<Button-3>', self.no_add)  # 右クリック
+        self.root.mainloop()
+
+    def select_image(self):
+        self.img_path = random.choice(self.image_scandir_list).path
+        img = Image.open(self.img_path)
+        img = ImageTk.PhotoImage(
+            img.resize((img.width//5, img.height//5)))
+        return img
+
     # 重複しないようなランダムなファイル名を生成する
-    randlst = [random.choice(string.ascii_letters + string.digits)
-               for i in range(10)]
-    return ''.join(randlst)
+    def random_name(self):
+        randlst = [random.choice(string.ascii_letters + string.digits)
+                   for i in range(10)]
+        return ''.join(randlst)
 
+    def next_img(self):
+        self.img = self.select_image()
+        self.canvas.create_image(0, 0, image=self.img, anchor=tk.NW)
 
-def next_img():
-    global img, img_path
-    img_path = random.choice(image_scandir_list).path
-    img = Image.open(img_path)
-    img = ImageTk.PhotoImage(img.resize((img.width//5, img.height//5)))
-    canvas.create_image(0, 0, image=img, anchor=tk.NW)
+    def add_image(self, event):
+        src = self.img_path
+        copy = os.path.join(TOPATH, self.random_name()+".png")
+        shutil.copyfile(src, copy)
+        print("add")
+        self.add_image_count += 1
+        self.next_img()
 
-
-def add_image(event):
-    src = img_path
-    copy = os.path.join(TOPATH, random_name()+".png")
-    shutil.copyfile(src, copy)
-    print("add")
-    next_img()
-
-
-def no_add(event):
-    print("pass")
-    next_img()
+    def no_add(self, event):
+        print("pass")
+        self.next_img()
 
 
 if __name__ == "__main__":
+    cf = CheckFace()
     try:
-        img_path = random.choice(image_scandir_list).path
-        img = Image.open(img_path)
-        img = ImageTk.PhotoImage(img.resize((img.width//5, img.height//5)))
-        canvas = tk.Canvas(
-            bg="black", width=2000, height=2000)
-        canvas.place(x=0, y=0)
-        canvas.create_image(0, 0, image=img, anchor=tk.NW)
-        root.geometry("{}x{}".format(800, 800))
-        canvas.bind('<Button-1>', add_image)  # 左クリック
-        canvas.bind('<Button-3>', no_add)  # 右クリック
-        root.mainloop()
+        cf()
     finally:
-        print("add image : {}".format)
+        print("add image : {}".format(cf.add_image_count))
+        print("image num : {} -> {}".format(cf.image_num, cf.current_image_num))
